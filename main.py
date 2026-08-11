ccfb2c09b5c6ea6675a4e1b9d3a2657d6ce28874
Saad Ksioui <saadksioui@gmail.com>
2026-08-11 18:09:19 +0100

    Add argument parsing for user input and verbose output

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from openai import OpenAI
 
@@ -15,17 +16,19 @@ client = OpenAI(
     api_key=api_key,
 )
 
-messages = [
-    {
-        "role": "user",
-        "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
-    }
-]
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 
+args = parser.parse_args()
+messages = [
+    {"role": "user", "content": args.user_prompt},
+]
 response = client.chat.completions.create(model="openrouter/free", messages=messages)
-print("User prompt: " + messages[0]["content"])
-print("Prompt tokens: " + str(response.usage.prompt_tokens))
-print("Response tokens: " + str(response.usage.completion_tokens))
+if args.verbose:
+    print("User prompt: " + args.user_prompt)
+    print("Prompt tokens: " + str(response.usage.prompt_tokens))
+    print("Response tokens: " + str(response.usage.completion_tokens))
 print("Response:")
 print(response.choices[0].message.content)
 
