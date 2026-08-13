25fb5947993cdb5f1d022faf33c4de18f93829dc
Saad Ksioui <saadksioui@gmail.com>
2026-08-13 21:39:54 +0100

    Add system prompt and available functions for file operations

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 import argparse
 from dotenv import load_dotenv
 from openai import OpenAI
+from prompts import system_prompt
+from functions.call_function import available_functions
+import json
 
 
 load_dotenv()
@@ -22,13 +25,21 @@ parser.add_argument("--verbose", action="store_true", help="Enable verbose outpu
 
 args = parser.parse_args()
 messages = [
+    {"role": "system", "content": system_prompt},
     {"role": "user", "content": args.user_prompt},
 ]
-response = client.chat.completions.create(model="openrouter/free", messages=messages)
+response = client.chat.completions.create(
+    model="openrouter/free",
+    messages=messages,
+    tools=available_functions
+)
 if args.verbose:
     print("User prompt: " + args.user_prompt)
     print("Prompt tokens: " + str(response.usage.prompt_tokens))
     print("Response tokens: " + str(response.usage.completion_tokens))
+for tool_call in response.choices[0].message.tool_calls:
+    function_args = json.loads(tool_call.function.arguments or "{}")
+    print(f"Calling function: {tool_call.function.name}({function_args})")
 print("Response:")
 print(response.choices[0].message.content)
 
